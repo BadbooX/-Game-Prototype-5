53c2e3305a221eb4e78f28b226d9f978f56c2d25
BadbooX <k.cotton.pro@gmail.com>
2025-06-30 16:28:28 +0200

    Add a game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Ajouter une list de prefabs pour la remplir avec les gameobjets
+    public List<GameObject> targets;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
