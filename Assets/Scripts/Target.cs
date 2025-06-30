a7881f0711ec0ef48a7e1df55a7ba4526b695a83
BadbooX <k.cotton.pro@gmail.com>
2025-06-30 15:56:49 +0200

    Initial commit for Prototype 5(UI)

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 4;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        // Speed of the target
+        targetRb.AddForce(RandomSpeed(), ForceMode.Impulse);
+        // Torque in various axis X            Y               Z
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        // SpawnPos
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Vector3 RandomSpeed()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
