f4da88b0b0e1c1de86431e23e14141d6416bacb3
BadbooX <k.cotton.pro@gmail.com>
2025-06-30 16:46:07 +0200

    Add a coroutine to spawn prefabs(which are in the List) randomly

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,15 +6,30 @@ public class GameManager : MonoBehaviour
 {
     // Ajouter une list de prefabs pour la remplir avec les gameobjets
     public List<GameObject> targets;
+    // Temps en seconde du spawn des prefabs
+    private float spawnRate = 1.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    { // Lance la coroutine
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    IEnumerator SpawnTarget()
+    {
+        // Attends spawnRate seconde 
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            // CHoisit un index aléatoire entre 0 et le nombre max de préfab dans la list
+            int index = Random.Range(0, targets.Count);
+            // SPawn
+            Instantiate(targets[index]);
+        }
     }
 }
